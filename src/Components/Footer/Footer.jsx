a32b3cafa2761ff8c376d256676fdf0b19cfe2ca
debhValois <debhvalois@gmail.com>
2023-07-05 15:29:54 -0300

    Day 04 - Footer: ajustes finos

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,19 @@
-import Logo from '@/UI/Logo';
-import SocialLinks from '@/UI/SocialLinks';
-import styles from './Footer.module.scss';
+import Logo from "@/UI/Logo";
+import SocialLinks from "@/UI/SocialLinks";
+import styles from "./Footer.module.scss";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <div className={styles.content}>
-        <div className={styles.logo}>
-            <Logo />
-           <p>
-             Transformando visitantes em clientes.
-           </p>
-         </div>
+      <div className={styles.rowLogo}>
+        <h1>
+          <Logo />
+        </h1>
+        <span>Transformando visitantes em clientes.</span>
       </div>
-      
       <div className={styles.content}>
         <div className={styles.columns}>
-          <h3>
-          Links Principais
-          </h3>
+          <h3>Links Principais</h3>
           <ul>
             <li>Home</li>
             <li>Ferramenta</li>
@@ -27,9 +22,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className={styles.columns}>
-          <h3>
-          Cases
-          </h3>
+          <h3>Cases</h3>
           <ul>
             <li>Geração de Leads B2B</li>
             <li>Geração de Leads em Software</li>
@@ -38,9 +31,7 @@ const Footer = () => {
           </ul>
         </div>
         <div className={styles.columns}>
-          <h3>
-          Materiais
-          </h3>
+          <h3>Materiais</h3>
           <ul>
             <li>Blog</li>
             <li>Parcerias com Agências</li>
@@ -49,22 +40,32 @@ const Footer = () => {
           </ul>
         </div>
         <div className={styles.columns}>
-          <h3>
-          Siga a Leadster
-          </h3>
+          <h3>Siga a Leadster</h3>
           <SocialLinks />
           <ul>
-            <p><b>E-mail:</b> contato@leadster.com.br</p>
-            <p><b>Telefone:</b> (42) 98828-9851</p>
+            <p>
+              <b>E-mail:</b> contato@leadster.com.br
+            </p>
+            <p>
+              <b>Telefone:</b> (42) 98828-9851
+            </p>
           </ul>
         </div>
-      </div >
+      </div>
       <div className={styles.content}>
-        <span>Copyright© 2015 - 2022 Todos os direitos reservados |<a href='#'> Leadster</a></span>
-        <span>Rua José Loureiro, 464 - Centro - Curitiba PR - CEP: 80010-000 |<a href='#'> Termos de Uso</a></span>
+        <span>
+          Copyright© 2015 - 2022 Todos os direitos reservados |
+          <a href="#">
+            {" "}
+            <b>Leadster</b>
+          </a>
+        </span>
+        <span>
+          Rua José Loureiro, 464 - Centro - Curitiba PR - CEP: 80010-000 |
+          <a href="#"> Termos de Uso</a>
+        </span>
       </div>
     </footer>
   );
 };
-
 export default Footer;
